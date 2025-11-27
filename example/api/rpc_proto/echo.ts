03a8f1db5c42e781eb1c36ea6b7c2a2a00920e5e
995933447 <995933447@example.com>
2025-11-27 14:34:16 +0400

    ok

--- example/api/rpc_proto/echo.ts
+++ example/api/rpc_proto/echo.ts
@@ -4,12 +4,12 @@ import api from './echo.d';
 
 export class EchoService {
 
-  /** no comment **/
+  /** 获取示例数据 */
   static async getExampleData(req: api.echo.IGetExampleDataReq): Promise<api.echo.IGetExampleDataRsp> {
     return await request.get('/v1/example/data', { params: req })
   };
 
-  /** no comment **/
+  /** 提交示例数据 */
   static async postExampleData(req: api.echo.IPostExampleDataReq): Promise<api.echo.IPostExampleDataRsp> {
     return await request.post('/postExampleData', req)
   };
